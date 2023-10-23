f2ef6813cbbdddf2e90a31fac158c90eb875235f
WalidAit69 <aitwalid2000@gmail.com>
2023-10-23 19:46:03 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -52,6 +52,9 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+app.get("/" , (req , res) =>{
+  res.send("home get request")
+})
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
